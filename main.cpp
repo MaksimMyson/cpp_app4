2ba2ee4d4194ff209863bcfa8f2b8b7e2051c286
MaksimMyson <maksimmyson33@gmail.com>
2023-10-23 20:03:19 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <conio.h>
 using namespace std;
 
 int main()
 {
+	const int size = 10;
+	int arr[size];
 	
+	cout << "Enter" << size << "integres:" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+	cout << "Array in reverse order:" << endl;
+
+	for (int i = size - 1; i >= 0; i--)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+
 	return 0;
 }
 
