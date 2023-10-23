de9e440c0aad2416db3785a77b09ac1ae3a38861
MaksimMyson <maksimmyson33@gmail.com>
2023-10-23 19:42:24 +0300

    ex1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
+	double montly_profit[6];
+	double total_profit = 0;
+
+	for ( int month = 0; month < 6; month++)
+	{
+		cout << "Enter the profit for month " << month + 1 << ": ";
+		cin >> montly_profit[month];
+		total_profit += montly_profit[month];
+	}
+	cout << "The total profit for the 6 months is: " << total_profit << endl;
 	
+
 	return 0;
 }
 
