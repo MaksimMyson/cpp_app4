884d34818d18889d22ddcc02b3b6aa2934014748
MaksimMyson <maksimmyson33@gmail.com>
2023-10-23 20:11:34 +0300

    ex3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
+	double sides[5];
+	double perimeter = 0;
+
+	cout << "Enter the lengths of the sides of the pentagon: ";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Side " << i + 1 << ": ";
+		cin >> sides[i];
+		perimeter += sides[i];
+	}
+
+	cout << "The perimeter of the pentagon is " << perimeter << endl;
+
+
+
+
 	
 	return 0;
 }
